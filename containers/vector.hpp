f73a521656434092ebdc58239e0eb830e26c8c56
aachbaro <aachbaro@student.42.fr>
2022-12-08 17:03:05 +0100

    regler le segfault

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -15,10 +15,10 @@ namespace ft
 
         typedef T                                   value_type;
         typedef Alloc                               allocator_type;
-        typedef allocator_type::reference           reference;
-        typedef allocator_type::const_reference     const_reference;
-        typedef allocator_type::pointer             pointer;
-        typedef allocator_type::const_pointer       const_pointer;
+        typedef typename allocator_type::reference           reference;
+        typedef typename allocator_type::const_reference     const_reference;
+        typedef typename allocator_type::pointer             pointer;
+        typedef typename allocator_type::const_pointer       const_pointer;
         typedef unsigned long                       size_type;
 
         private :
@@ -26,17 +26,26 @@ namespace ft
         T*              _array;
         size_type       _capacity;
         size_type       _size;
+        allocator_type  _allocator;
 
         public :
         
         vector(): _array(nullptr), _capacity(0), _size(0) {}
         explicit vector(size_type n, const value_type& val) {
-            _array = new value_type[n * 2];
+            _array = _allocator.allocate(n);
             for (int i = 0; i != n; i++) { _array[i] = val; }
+            _size = n;
         }
-        ~vector() { delete [] _array; }
+        ~vector() { _allocator.deallocate(_array, _size); }
 
-        T &operator[](size_type i) { return (_array[i]); }
+        T &operator[](size_type i) { return (*(_array + i)); }
+
+        reference           at(size_type position) { return *(_array + position); }
+        const_reference     at(size_type position) const { return *(_array + position); }
+        reference           back() { return *(_array + _size); }
+        const_reference     back() const { return *(_array + _size); }
+
+        size_type           size() const { return(_size); }
 
         /* iterators */
         
--- main.cpp
+++ main.cpp
@@ -4,15 +4,30 @@
 #include <iostream>
 #include <vector>
 
+void    printab(ft::vector<int> tab)
+{
+    std::cout << "-- tab data --" << std::endl;
+    std::cout << "size = " << tab.size() << std::endl;
+    for (size_t i = 0; i < tab.size(); i++)
+    {
+        std::cout << tab.at(i) << " | ";
+    }
+    std::cout << std::endl;   
+}
+
 int main(void)
 {
     ft::vector<int> tab(5, 4);
     std::vector<int> tab2(5, 4);
 
-    for (size_t i = 0; i < 5; i++)
-    {
-        std::cout << tab[2] << std::endl;        
-    }
+
+    printab(tab);
+    tab.at(3) = 3;
+    printab(tab);
+    tab.back() = 10;
+    printab(tab);
+
+
     std::cout << "capacity  : " << tab2.capacity() << std::endl;
     std::cout << "size      : " << tab2.max_size() << std::endl;
     
