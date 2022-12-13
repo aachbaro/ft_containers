1b103c1223591e764b00b971c3bc3ceaf2d28c7f
aachbaro <aachbaro@student.42.fr>
2022-12-13 17:47:57 +0100

    regler popback

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -13,13 +13,13 @@ namespace ft
     {
         public :
 
-        typedef T                                   value_type;
-        typedef Alloc                               allocator_type;
-        typedef typename allocator_type::reference           reference;
-        typedef typename allocator_type::const_reference     const_reference;
-        typedef typename allocator_type::pointer             pointer;
-        typedef typename allocator_type::const_pointer       const_pointer;
-        typedef unsigned long                       size_type;
+        typedef T                                           value_type;
+        typedef Alloc                                       allocator_type;
+        typedef typename allocator_type::reference          reference;
+        typedef typename allocator_type::const_reference    const_reference;
+        typedef typename allocator_type::pointer            pointer;
+        typedef typename allocator_type::const_pointer      const_pointer;
+        typedef unsigned long                               size_type;
 
         private :
 
@@ -35,17 +35,42 @@ namespace ft
             _array = _allocator.allocate(n);
             for (int i = 0; i != n; i++) { _array[i] = val; }
             _size = n;
+            _capacity = n;
         }
         ~vector() { _allocator.deallocate(_array, _size); }
 
         T &operator[](size_type i) { return (*(_array + i)); }
 
-        reference           at(size_type position) { return *(_array + position); }
-        const_reference     at(size_type position) const { return *(_array + position); }
-        reference           back() { return *(_array + _size); }
-        const_reference     back() const { return *(_array + _size); }
+        reference           at(size_type position) { return (_array[position]); }
+        const_reference     at(size_type position) const { return (_array[position]); }
+        reference           back() { return (_array[_size - 1]); }
+        const_reference     back() const { return (_array[_size - 1]); }
+        reference           front() { return (_array[0]); }
+        const_reference     front() const { return (_array[0]); }
+        T*                  data() { return (_array); }
+        const T*            data() const { return (_array); }
+        bool                empty() const { return (_size == 0 ? 1 : 0); }
+        size_type           max_size() const { return (allocator_type().max_size()); }
+        void                reserve(size_type new_cap) {
+            _array = _allocator.allocate(new_cap);
+            _capacity = new_cap;
+        }
+        void                clear() {
+            for (size_type i = 0; i < _size; i++) { _allocator.destroy(&at(i)); }
+            _size = 0;
+        }
+        void                push_back(const T& value) {
+            if (_capacity == _size) { reserve(_capacity + 1); }
+            _size++;
+            back() = value;
+        }
+        void                pop_back() {
+            _allocator.destroy(&back());
+            _size--;
+        }
 
         size_type           size() const { return(_size); }
+        size_type           capacity() const { return (_capacity); }
 
         /* iterators */
         
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <vector>
 
-void    printab(ft::vector<int> tab)
+void    printab(ft::vector<int> const &tab)
 {
     std::cout << "-- tab data --" << std::endl;
     std::cout << "size = " << tab.size() << std::endl;
@@ -15,21 +15,39 @@ void    printab(ft::vector<int> tab)
     std::cout << std::endl;   
 }
 
+void pointer_func(const int* p, unsigned long size)
+{
+    std::cout << "data = ";
+    for (std::size_t i = 0; i < size; ++i)
+        std::cout << p[i] << ' ';
+    std::cout << '\n';
+}
+
 int main(void)
 {
     ft::vector<int> tab(5, 4);
     std::vector<int> tab2(5, 4);
 
 
-    printab(tab);
+    //printab(tab);
     tab.at(3) = 3;
-    printab(tab);
+    //printab(tab);
     tab.back() = 10;
-    printab(tab);
+    //printab(tab);
+    tab.front() = 0;
+    //printab(tab);
+    pointer_func(tab.data(), tab.size());
+    std::cout << tab.max_size() << std::endl;
+    std::cout << tab.size() << std::endl;
+    tab.reserve(28);
+    pointer_func(tab.data(), tab.size());
+    tab.clear();
+    std::cout << tab.capacity() << std::endl;
+    std::cout << tab.size() << std::endl;
+    tab.push_back(8);
+    tab.push_back(456);
+    tab.pop_back();
 
-
-    std::cout << "capacity  : " << tab2.capacity() << std::endl;
-    std::cout << "size      : " << tab2.max_size() << std::endl;
-    
+    pointer_func(tab.data(), tab.size());
     return (0);
 }
